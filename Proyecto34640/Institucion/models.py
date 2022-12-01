1422e78b05a74cdd73a8e24028e6e806e68d52a1
Facundo Mercado <facu.mercado1224@gmail.com>
2022-11-30 23:28:00 -0300

    Primer commit

---
+++ Proyecto34640/Institucion/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from Institucion.models import *
+
+# Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length = 40)
+    apellido = models.CharField(max_length = 40)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length = 40)
+    apellido = models.CharField(max_length = 40)
+    email = models.EmailField()
+    materiaQueDa = models.CharField(max_length = 40)
+
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length = 40)
+    aprobada = models.BooleanField()
